811e095582d721754d97f74a82fadcd510ba10bd
Zhong Ting Qiang <2861315501@qq.com>
2025-03-13 19:52:38 +0800

    1. SV配置和组件封装

---
+++ src/components/SvgIcon/index.vue
@@ -0,0 +1,41 @@
+<script lang='ts' setup>
+    defineProps({
+        // 图标名前缀
+        prefix: {
+            type: String,
+            default: '#icon-'
+        },
+        // 图标名
+        name: {
+            type: String,
+            default: 'cap',
+        },
+        // 图标颜色
+        color: {
+            type: String,
+            default: '',
+        },
+        // 图标宽度
+        width: {
+            type: String,
+            default: '16px'
+        },
+        // 图标高度
+        height: {
+            type: String,
+            default: '16px'
+        }
+    })
+</script>
+
+<template>
+    <div class=''>
+        <!-- svg:图标外层容器节点 内部需要与use标签结合使用 -->
+        <svg :style="{ width, height }">
+            <!-- 指定图标名字，需以icon开头 -->
+            <use :xlink:href="prefix + name" fill=""></use>
+        </svg>
+    </div>
+</template>
+
+<style scoped></style>
