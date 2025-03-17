340d6e950d20b036cdc1eb4b2b5b993af2343978
Zhong Ting Qiang <2861315501@qq.com>
2025-03-17 18:19:40 +0800

    14. 左侧菜单静态搭建

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -7,6 +7,22 @@
         <!-- 左侧菜单 -->
         <div class="layout_slider">
             <Logo></Logo>
+            <!-- 导航菜单 -->
+            <el-scrollbar class="scrollbar">
+                <el-menu background-color="$base-menu-background" text-color="white">
+                    <el-menu-item index="1">首页</el-menu-item>
+                    <el-menu-item index="2">数据大屏</el-menu-item>
+                    <!-- 多级菜单 -->
+                    <el-sub-menu index="3">
+                        <template #title>
+                            <span>权限管理</span>
+                        </template>
+                        <el-menu-item index="3-1">用户管理</el-menu-item>
+                        <el-menu-item index="3-2">角色管理</el-menu-item>
+                        <el-menu-item index="3-3">菜单管理</el-menu-item>
+                    </el-sub-menu>
+                </el-menu>
+            </el-scrollbar>
         </div>
 
         <!-- 顶部导航 -->
@@ -29,6 +45,11 @@
             width: $base-menu-width;
             height: 100vh;
             background-color: $base-menu-background;
+
+            .scrollbar {
+                width: 100%;
+                height: calc(100vh - $base-menu-logo-hegiht);
+            }
         }
 
         .layout_tabbar {
